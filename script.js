c27f8df1c638d878161cc489afb2515d811ac15a
Natalia_Lapina <sumrakn@icloud.com>
2024-04-22 13:36:40 +0200

    add functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let counter = 0;
 
+const increaseBtn = document.querySelector('.increase');
+const decreaseBtn = document.querySelector('.decrease');
+const resetBtn = document.querySelector('.reset');
+const result = document.querySelector('.result');
+
+increaseBtn.addEventListener('click', function () {
+  counter += 1;
+  result.innerHTML = counter;
+});
+
+decreaseBtn.addEventListener('click', function () {
+  counter -= 1;
+  result.innerHTML = counter;
+});
+
+resetBtn.addEventListener('click', function () {
+  counter = 0;
+  result.innerHTML = counter;
+});
