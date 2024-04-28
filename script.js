edfc0e3d11e3c6914d323713dfd692cf0d648571
Anton Shekhov <shekhov.anton@gmail.com>
2024-04-28 21:18:47 +0200

    great job!

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// Not much to note here, all looks good!
+// For improvements purposes, you could switch from alerting (its very intrusive function, mostly used for debugging purposes)
+// to changing your html from the script. Like adding a text somewhere, or changing the color of the background.
+// You can also disable the + button completely, when the counter reaches the top limit
 let counter = 0;
 const topLimit = 5;
 
+// Great job caching your DOM elements
 const increaseBtn = document.querySelector('.increase');
 const decreaseBtn = document.querySelector('.decrease');
 const resetBtn = document.querySelector('.reset');
